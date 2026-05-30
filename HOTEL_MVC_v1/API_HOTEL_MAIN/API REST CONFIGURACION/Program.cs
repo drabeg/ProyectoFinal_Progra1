0424c15bdf079726e719c8fe8d04fa3a8b38ad98
Dario Rabe <rabedario@gmail.com>
2026-05-30 01:00:52 -0600

    Configuración final de motor MySQL para producción en Railway

--- HOTEL_MVC_v1/API_HOTEL_MAIN/API REST CONFIGURACION/Program.cs
+++ HOTEL_MVC_v1/API_HOTEL_MAIN/API REST CONFIGURACION/Program.cs
@@ -16,7 +16,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<HotelDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 
 var app = builder.Build();
 
